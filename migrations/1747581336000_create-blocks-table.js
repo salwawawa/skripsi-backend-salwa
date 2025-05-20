cfdf632951dfff02cbd3a3ccf78acd0a6263f9ee
Dhanuwrdhn <157359379+Dhanuwardhan@users.noreply.github.com>
2025-05-20 11:58:16 +0700

    sasa

--- migrations/1747581336000_create-blocks-table.js
+++ migrations/1747581336000_create-blocks-table.js
@@ -1,12 +1,5 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
 exports.shorthands = undefined
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @returns {Promise<void> | void}
- */
 exports.up = (pgm) => {
   pgm.createTable('blocks', {
     id: {
@@ -33,26 +26,8 @@ exports.up = (pgm) => {
       default: pgm.func('current_timestamp'),
     },
   })
-
-  // Add block_id to shops table
-  pgm.addColumn('shops', {
-    block_id: {
-      type: 'integer',
-      notNull: true,
-      references: 'blocks',
-      onDelete: 'restrict',
-    },
-  })
-
-  // Add index for better performance
-  pgm.createIndex('shops', 'block_id')
 }
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- */
 exports.down = (pgm) => {
-  pgm.dropIndex('shops', 'block_id')
-  pgm.dropColumn('shops', 'block_id')
   pgm.dropTable('blocks')
 }
